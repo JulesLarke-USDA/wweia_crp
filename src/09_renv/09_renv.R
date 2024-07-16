036f1614c17987ce4e1c036e89334cba1143ee92
jules.larke <jules.larke@usda.gov>
2024-07-16 14:38:36 -0700

    add R version control and package versions

---
+++ src/09_renv/09_renv.R
@@ -0,0 +1,7 @@
+## Title: R environment version control
+## Author: Jules Larke
+## Date: 071624
+## Purpose: Create a snapshot of package versions used for reproducing the analyses in WWEIA CRP
+
+renv::init()
+renv::snapshot()
